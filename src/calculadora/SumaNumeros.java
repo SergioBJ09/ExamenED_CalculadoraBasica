0e0d73638323480ad480f7b02a97f17778e3e3be
SergioBJ09 <sbolivar19332@iesjoanramis.org>
2024-12-12 18:37:55 +0100

    Creacion del archivo SumaNumeros.java

---
+++ src/calculadora/SumaNumeros.java
@@ -0,0 +1,19 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class SumaNumeros {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // Declaracion de la Clase Scanner
+
+		System.out.print("Ingresa el primer número: ");
+		int numero1 = scanner.nextInt(); // Input del primer numero
+		
+		System.out.print("Ingresa el segundo número: ");
+		int numero2 = scanner.nextInt(); // Input del segundo numero
+		
+		int suma = numero1 + numero2; // Suma de numeros
+		
+		System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma); // Resultado
+	}
+}
